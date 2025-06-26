0f1f95fd37047337942232561e8d261429905ba0
TWO-ONE-21 <ardorianda1809@gmail.com>
2025-06-27 02:42:44 +0700

    A desktop application developed for an OOP course final project, featuring a Java Swing frontend and MySQL backend.

---
+++ src/main/java/com/mycompany/managementkaskelas/ManagementKasKelas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.managementkaskelas;
+
+/**
+ *
+ * @author ACER
+ */
+public class ManagementKasKelas {
+
+    public static void main(String[] args) {
+        new main().show();
+    }
+}
